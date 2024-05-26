f3099694bdd39023f8a27524a7f4b5cef74dca7f
zxm-3208 <zhou-xueming@foxmail.com>
2024-05-26 22:07:14 +0800

    chat服务初始化

---
+++ douyin_chat/src/main/java/com/example/douyin_chat/DouyinChatApplication.java
@@ -0,0 +1,21 @@
+package com.example.douyin_chat;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+@MapperScan("com.example.douyin_chat.mapper.**")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class DouyinChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DouyinChatApplication.class, args);
+    }
+
+}
---
+++ douyin_chat/src/test/java/com/example/douyin_chat/DouyinChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.douyin_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DouyinChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
